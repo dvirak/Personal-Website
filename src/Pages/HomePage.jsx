6c040b19e28545f44f631447385a83ab2a46599d
dvirak <dvirak43@vt.edu>
2024-03-24 18:09:07 -0400

    Initial Testimonials Layout and CSS

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,7 +2,7 @@
 import Navbar from "../Components/Navbar";
 import Services from "../Components/Services";
 import HomeSection from "../Components/HomeSection";
-import Testimonial from "../Components/Testimonial";
+import Testimonial from "../Components/Testimonial/Testimonial";
 import Work from "../Components/Work/Work";
 
 export default function Home() {
