3c61c0b63c732e61284e3d9148dae4ebb4045044
dvirak <dvirak43@vt.edu>
2024-03-31 12:48:00 -0400

    Email Function

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,11 +1,8 @@
 //! Imported Components/Variables----------------
+import SendEmail from "../Components/Contact/Email";
 import Navbar from "../Components/Navbar";
 
 export default function Contact() {
-  const pathname = window.location.pathname;
-
-  console.log(pathname);
-
   return (
     <>
       <Navbar className="header-pages" />
@@ -16,59 +13,7 @@ export default function Contact() {
         </h2>
 
         <div className="contact__page container grid">
-          <form action="" className="contact__form grid" id="contact-form">
-            <div className="contact__group grid">
-              <div className="contact__box">
-                <input
-                  type="text"
-                  name="user_name"
-                  id="name"
-                  required
-                  placeholder="Write your name"
-                  className="contact__input"
-                  autoComplete="name"
-                ></input>
-                <label htmlFor="name" className="contact__label">
-                  Name
-                </label>
-              </div>
-
-              <div className="contact__box">
-                <input
-                  type="text"
-                  name="user_email"
-                  id="email"
-                  required
-                  placeholder="Write your email"
-                  className="contact__input"
-                ></input>
-                <label htmlFor="email" className="contact__label">
-                  Email Address
-                </label>
-              </div>
-            </div>
-
-            <div className="contact__box contact__area">
-              <textarea
-                name="user_message"
-                id="message"
-                required
-                placeholder="Write your message"
-                className="contact__input"
-              ></textarea>
-              <label htmlFor="message" className="contact__label">
-                Message
-              </label>
-            </div>
-
-            <p className="contact__message" id="contact-message">
-              Message
-            </p>
-
-            <button type="submit" className="contact__send button">
-              Send Message
-            </button>
-          </form>
+          <SendEmail />
         </div>
       </section>
     </>
