38d57073f0590bf2acfc06c9fa051158c1304f4e
dvirak <dvirak43@vt.edu>
2024-04-03 20:15:19 -0400

    Finished Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,22 +10,48 @@ export default function Footer() {
           </Link>
           <ul className="footer__links">
             <li>
-              <Link to="/About" className="foot_link">
+              <Link to="/About" className="footer__link">
                 About Me
               </Link>
             </li>
             <li>
-              <Link to="Portfolio" className="foot_link">
+              <Link to="Portfolio" className="footer__link">
                 Portfolio
               </Link>
             </li>
             <li>
-              <Link to="/Contact" className="foot_link">
+              <Link to="/Contact" className="footer__link">
                 Contact Me
               </Link>
             </li>
           </ul>
+
+          <div className="footer__social">
+            <Link
+              to="https://www.linkedin.com/in/danielvirak/"
+              target="_blank"
+              className="footer__social-link"
+            >
+              <i className="ri-linkedin-box-fill"></i>
+            </Link>
+
+            <Link
+              to="https://github.com/dvirak"
+              target="_blank"
+              className="footer__social-link"
+            >
+              <i className="ri-github-fill"></i>
+            </Link>
+
+            {/* <Link to="" target="_blank" className="footer__social-link"></Link> */}
+
+            {/* <Link to="" target="_blank" className="footer__social-link"></Link> */}
+          </div>
         </div>
+
+        <span className="footer__copy">
+          &#169; Daniel Virak 2024, All Rights Reserved
+        </span>
       </div>
     </footer>
   );
